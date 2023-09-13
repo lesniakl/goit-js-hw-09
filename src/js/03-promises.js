b0c08c499e531cc707ccdf34eeb7b91371fea1a7
Łukasz Leśniak <lukaszlesniaksw@gmail.com>
2023-09-13 17:51:05 +0200

    task 3 complete

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,8 +1,44 @@
+import Notiflix from 'notiflix';
+
+const generator = document.querySelector('.form');
+
+generator.addEventListener('submit', generate);
+
+function generate(e) {
+  e.preventDefault();
+  const delay = parseInt(document.querySelector("input[name='delay']").value);
+  const step = parseInt(document.querySelector("input[name='step']").value);
+  const amount = parseInt(document.querySelector("input[name='amount']").value);
+  for (let i = 1; i <= amount; i++) {
+    createPromise(i, delay + step * (i - 1))
+      .then(({ position, delay }) => {
+        Notiflix.Notify.success(
+          `✅ Fulfilled promise ${position} in ${delay}ms`
+        );
+      })
+      .catch(({ position, delay }) => {
+        Notiflix.Notify.failure(
+          `❌ Rejected promise ${position} in ${delay}ms`
+        );
+      });
+  }
+}
+
 function createPromise(position, delay) {
   const shouldResolve = Math.random() > 0.3;
   if (shouldResolve) {
-    // Fulfill
+    const promise = new Promise(resolve => {
+      setTimeout(() => {
+        resolve({ position: position, delay: delay });
+      }, delay);
+    });
+    return promise;
   } else {
-    // Reject
+    const promise = new Promise((resolve, reject) => {
+      setTimeout(() => {
+        reject({ position: position, delay: delay });
+      }, delay);
+    });
+    return promise;
   }
 }
