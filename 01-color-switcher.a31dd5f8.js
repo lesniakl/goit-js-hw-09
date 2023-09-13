cce518e4bcde5ca8bfb63037527ffafaf960ea3f
lesniakl <126507475+lesniakl@users.noreply.github.com>
2023-09-13 15:16:30 +0000

    Deploying to gh-pages from @ lesniakl/goit-js-hw-09@48f77dc5debba66ec0469a8015c8fc3f8b9c70c9 🚀

---
+++ 01-color-switcher.a31dd5f8.js
@@ -0,0 +1,2 @@
+!function(){var t=document.querySelector("body"),e=document.querySelector("button[data-start]"),r=document.querySelector("button[data-stop]"),o=0;e.addEventListener("click",(function(e){e.target.toggleAttribute("disabled"),r.removeAttribute("disabled"),o=setInterval((function(){return t.style.backgroundColor="#".concat(Math.floor(16777215*Math.random()).toString(16))}),1e3)})),r.addEventListener("click",(function(t){t.target.toggleAttribute("disabled"),e.removeAttribute("disabled"),clearInterval(o)}))}();
+//# sourceMappingURL=01-color-switcher.a31dd5f8.js.map
